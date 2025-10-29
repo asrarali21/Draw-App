3a12989260cd500add7ecf6c8a9806bb645620ab
asrarali21 <mohdasrar3825@gmail.com>
2025-10-29 21:02:11 +0530

    feat(http-backend): add use r controller and update tsconfig; modify dev script

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,4 +1,13 @@
-import ws from "ws";
+import { WebSocketServer } from "ws";
 
 
 
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+    ws.send("pong")
+  });
+});
